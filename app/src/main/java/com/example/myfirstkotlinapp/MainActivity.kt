a8b03949d766e0141a005558551a616572e88ea3
NithyaYamsinghe <nitheeromeshi@gmail.com>
2020-05-23 21:45:28 +0530

    firstcommit

---
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myfirstkotlinapp
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+// IT18233704 N.R Yamasinghe
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun onSubmit(view: View) {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        txtAge.text = "Age :" + (currentYear - inputYear.text.toString().toInt())
+    }
+
+}
